3a3d8e84952c80cbba3f36ad74f3ce908be1bddb
mrm0237 <mcleodriggs@gmail.com>
2026-09-24 14:51:21 -0500

    Simplify main function by removing argument checks
    Removed command line argument handling and simplified main function.

--- main.cpp
+++ main.cpp
@@ -24,13 +24,8 @@ double debt_finder(double cost, double m_interest_rate, double m_payments)
     return balance;
 }
 
-int main( int argc, char * argv[] )
+int main()
 {
-	if (argc > 4) 
-	{
-		cout << "Too many arguments. Cannot pass in more than three." << endl;
-		return -1;
-	}
 	// Define variables
 	int i = 1;
 	double loan_amount, yearly_interest_rate, monthly_payment;
@@ -57,30 +52,6 @@ int main( int argc, char * argv[] )
 	cout << "Monthly Payments: ";
 	cin >> monthly_paid;
 	
-
-	
-	if (argc > 1)
-	{
-		while ( i < argc )
-		{
-
-			try
-			{
-				arguments[i-1] = stod(argv[i]);
-			}
-			catch(const std::invalid_argument&)
-			{
-				if(i==1)
-					cout << "(Invalid loan amount): " << argv[i] << endl;
-				else if (i==2)
-					cout << "(Invalid interest rate): " << argv[i-1] << " " << argv[i] << endl;
-				else
-					cout << "(Invalid payment): " << argv[i-2] << " " << argv[i-1] << " " << argv[i] << endl;
-				return -2;
-			}
-			i++;
-		}
-	}
 
 	loan_amount = arguments[0];
 	yearly_interest_rate = arguments[1];
